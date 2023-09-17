50db71edba6fed5b19082b04aaf70a2dc87220ce
Deivison Resende <deivison.resende@enacom.com.br>
2023-09-17 19:35:42 -0300

    feat:Convert video/mp4 to audio/mp3 using ffmpeg

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,10 +3,13 @@ import { FileVideo, Upload } from "lucide-react";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import { loadFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 
 export function VideoInputForm (){
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>){
     const { files } = event.currentTarget
@@ -17,8 +20,53 @@ export function VideoInputForm (){
     setVideoFile(selectedFile)
   }
 
+  async function convertVideoToAudio(video: File){
+    console.log('convert started')
+
+    const ffmpeg = await loadFFmpeg()
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+    ffmpeg.on('progress', progress => {
+      console.log('progress: ', Math.round(progress.progress * 100))
+    })
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3'
+    ])
+
+    const data = await ffmpeg.readFile('output.mp3')
+
+    const audioFileBlob = new Blob([data], { type: 'audio/mpeg' })
+    const audioFile = new File([audioFileBlob], 'audio.mp3', { type: 'audio/mpeg' })
+
+    console.log('convert finished')
+
+    return audioFile
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if(!videoFile) return
+
+    const audioFile = await convertVideoToAudio(videoFile)
+
+    console.log('audioFile', audioFile, 'prompt', prompt)
+  }
+
   return (
-    <form action="" className="space-y-6">
+    <form onSubmit={handleUploadVideo} className="space-y-6">
       <label 
         htmlFor="video"
         className="border relative flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/10"
@@ -47,7 +95,8 @@ export function VideoInputForm (){
 
       <div className='space-y-2'>
         <Label htmlFor='transcription_prompt'>Prompt de transcrição</Label>
-        <Textarea 
+        <Textarea
+          ref={promptInputRef}
           id="transcription_prompt" 
           className='h-20 leading-relaxed resize-none' 
           placeholder='Inclua palavras chaves mencionadas no vídeo separadas por vírgula (,)'
